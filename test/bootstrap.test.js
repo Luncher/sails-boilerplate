375768d050cc272873f57a402f2e972120112761
chen <gakiclin@gmail.com>
2018-03-20 23:35:19 +0800

    setup test struture

---
+++ test/bootstrap.test.js
@@ -0,0 +1,43 @@
+const sails = require('sails')
+const EventProxy = require('eventproxy')
+const article = require('./fixtures/article')
+
+function setupFixtures(onDone) {
+  const ep = new EventProxy()
+  ep.all('article', onDone)
+  ep.fail(onDone)
+
+  article.setup(ep.done('article'))
+
+  return
+}
+
+function teardownFixtures(onDone) {
+  const ep = new EventProxy()
+  ep.all('article', onDone)
+  ep.fail(onDone)
+
+  article.teardown(ep.done('article'))
+
+  return
+}
+
+before(function(done) {
+  sails.lift({
+    // configuration for testing purposes
+  }, function(err) {
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    setupFixtures(function (err) {
+      done(err, sails);      
+    })
+  });
+
+  after(function(done) {
+    // here you can clear fixtures, etc.
+    teardownFixtures(function (err) {
+      sails.lower(done);      
+    })
+  });
+})
+
---
+++ test/fixtures/article.js
@@ -0,0 +1,35 @@
+const EventProxy = require('eventproxy')
+
+module.exports.setup = function(onDone) {
+  const ep = new EventProxy()
+
+  ep.all('one', 'two', function(articleOne, articleTwo) {
+    stubInstance.articleOneId = articleOne.data
+    stubInstance.articleTwoId = articleTwo.data
+    onDone()
+  })
+  ep.fail(onDone)
+
+  //TODO FIXME
+  ep.done('one')
+  ep.done('two')
+
+  return
+}
+
+module.exports.teardown = function(onDone) {
+  const ep = new EventProxy()
+  ep.all('one', 'two', function() {
+    onDone()
+  })
+  ep.fail(onDone)
+
+  //TODO FIXME
+  ep.done('one')
+  ep.done('two')
+
+  return
+}
+
+const stubInstance = {}
+module.exports.stubInstance = stubInstance
